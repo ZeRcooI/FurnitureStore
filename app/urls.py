bea2d10fd7170b65a81461d2b6f935bb8886b60b
MIKHAIL PANTELEEV <mihanpan1996@mail.ru>
2025-06-07 16:01:46 +0300

    подключено новое приложение goods и подключено к main

--- app/urls.py
+++ app/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from main import views
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
-    path('about/', views.about, name='about'),
+    path('', include('main.urls', namespace='main')),
+    path('catalog/', include('goods.urls', namespace='catalog')),
 ]
