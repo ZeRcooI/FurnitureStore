491343e6fda67463d2c37c01edf2c09962e8897f
MIKHAIL PANTELEEV <mihanpan1996@mail.ru>
2025-06-07 20:38:20 +0300

    перезалив бд + скрипт для выгрузкис бд

--- dump_utf8.py
+++ dump_utf8.py
@@ -3,6 +3,10 @@ import django
 from django.core.management import call_command
 import io
 
+# для запуска надо написать - python dump_utf8.py
+# для заливки из json в бд, надо написать - python manage.py loaddata fixtures/goods/cats.json
+
+
 # Настройка Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 django.setup()
@@ -10,6 +14,10 @@ django.setup()
 # Убедимся, что папка существует
 os.makedirs('fixtures/goods', exist_ok=True)
 
-# Сохраняем дамп в UTF-8
+# Сохраняем Categories в cats.json
 with io.open('fixtures/goods/cats.json', 'w', encoding='utf-8') as f:
     call_command('dumpdata', 'goods.Categories', stdout=f)
+
+# Сохраняем Products в prod.json
+with io.open('fixtures/goods/prod.json', 'w', encoding='utf-8') as f:
+    call_command('dumpdata', 'goods.Products', stdout=f)
