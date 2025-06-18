a662d700396274b11f354d9b3201ae715bd9cb61
MIKHAIL PANTELEEV <mihanpan1996@mail.ru>
2025-06-18 12:38:03 +0300

    добавлена пагинация + фильтр по товарам

--- goods/views.py
+++ goods/views.py
@@ -1,3 +1,5 @@
+from email.policy import default
+
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_list_or_404
 from goods.models import Products
@@ -6,11 +8,19 @@ from goods.models import Products
 def catalog(request, category_slug):
 
     page = request.GET.get('page', 1)
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)
 
     if category_slug == 'all':
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+
+    if order_by and order_by != 'default':
+        goods = goods.order_by(order_by)
 
     paginator = Paginator(goods, 3)
     current_page = paginator.page(int(page))
