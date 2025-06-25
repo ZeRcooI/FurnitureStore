1397a1df410982850de0c0b59cfbd4448adc90ba
MIKHAIL PANTELEEV <mihanpan1996@mail.ru>
2025-06-25 23:51:45 +0300

    добавлена визуализация корзины на страницах + её подсчет товаров и количества

--- carts/urls.py
+++ carts/urls.py
@@ -4,7 +4,7 @@ from carts import views
 app_name = 'carts'
 
 urlpatterns = [
-    path('cart_add/<int:product_id>/', views.cart_add, name='card_add'),
-    path('cart_change/<int:product_id>/', views.card_change, name='card_change'),
-    path('cart_remove/<int:product_id>/', views.cart_remove, name='cart_remove'),
+    path('cart_add/<slug:product_slug>/', views.cart_add, name='cart_add'),
+    path('cart_change/<slug:product_slug>/', views.card_change, name='cart_change'),
+    path('cart_remove/<slug:product_slug>/', views.cart_remove, name='cart_remove'),
 ]
