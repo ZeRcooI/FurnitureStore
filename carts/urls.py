daa9e0e68593af66c2eb725ad3a11fb2ff614781
MIKHAIL PANTELEEV <mihanpan1996@mail.ru>
2025-06-26 21:21:09 +0300

    СДЕЛАНО ИЗМЕНЕНИЕ АЯКС ДЛЯ  добавления + и - у товара

--- carts/urls.py
+++ carts/urls.py
@@ -5,6 +5,6 @@ app_name = 'carts'
 
 urlpatterns = [
     path('cart_add/', views.cart_add, name='cart_add'),
-    path('cart_change/', views.card_change, name='cart_change'),
+    path('cart_change/', views.cart_change, name='cart_change'),
     path('cart_remove/', views.cart_remove, name='cart_remove'),
 ]
