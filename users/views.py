e8eec13f8105856db82871e5f1745bd506268017
MIKHAIL PANTELEEV <mihanpan1996@mail.ru>
2025-06-27 01:24:35 +0300

    добавлено в профиле история заказов юзера

--- users/views.py
+++ users/views.py
@@ -1,11 +1,14 @@
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
+from django.db.models import Prefetch
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.template.context_processors import request
 from django.urls import reverse
 
+import orders
 from carts.models import Cart
+from orders.models import Order, OrderItem
 from users.forms import UserLoginForm, UserRegistrationForm, ProfileForm
 
 
@@ -78,9 +81,20 @@ def profile(request):
     else:
         form = ProfileForm(instance=request.user)
 
+    orders = (
+        Order.objects.filter(user=request.user).prefetch_related(
+            Prefetch(
+                'orderitem_set',
+                queryset=OrderItem.objects.select_related('product'),
+            )
+        )
+        .order_by('-id')
+    )
+
     context = {
         'title': 'HOME - Кабинет',
-        'form': form
+        'form': form,
+        'orders': orders,
     }
     return render(request, 'users/profile.html', context)
 
