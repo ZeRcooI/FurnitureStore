a0daabda6bd230afa16790a630aeff4269bd2012
MIKHAIL PANTELEEV <mihanpan1996@mail.ru>
2025-06-27 22:44:19 +0300

    микро

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 # from django.views.decorators.cache import cache_page
 
 from main import views
@@ -6,7 +8,7 @@ from main import views
 app_name = 'main'
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('', cache_page(60)(views.IndexView.as_view()), name='index'),
     path('about/', views.AboutView.as_view(), name='about'),
 ]
 
